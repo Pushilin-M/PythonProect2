9c17c542e014310311a5c858a2be4ef21af880d6
Pushilin-M <mvpooh@gmail.com>
2024-10-02 20:06:04 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,10 +1,3 @@
-data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
 
 def calculate_structure_sum(*args):
     s = 0
@@ -23,6 +16,13 @@ def calculate_structure_sum(*args):
     return s
 
 
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
 result = calculate_structure_sum(data_structure)
 print(result)
 
